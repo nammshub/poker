3a12dfd79698eaadcbb9b729ff5e5bf63ca70791
Iron_Dams <dams87150@gmail.com>
2018-08-30 22:27:41 +0200

    v0.1
    Création de la V0.1, initialisation des class

---
+++ Player.js
@@ -0,0 +1,25 @@
+class Player {
+  constructor(playerId, name, chips, state, dealer) {
+    this.playerId = playerId;
+    this.name = name;
+    this.chips = chips;
+    this.state = state;
+    defaultStatus(dealer);
+  }
+
+  get totalChips() {
+    return this.chips;
+  }
+
+  get dealerStatus() {
+    return this.dealer;
+  }
+
+  get stateStatus() {
+    return this.state;
+  }
+
+  set dealerStatus(dealer) {
+    this.dealer = dealer;
+  }
+}
