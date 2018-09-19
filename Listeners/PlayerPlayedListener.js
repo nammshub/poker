86c2950267c981d45586ffd7d8af46a166ac85ec
nammshub <vincent.dhainaut@gmail.com>
2018-09-19 18:03:59 +0200

    Update PlayerPlayedListener.js

--- Listeners/PlayerPlayedListener.js
+++ Listeners/PlayerPlayedListener.js
@@ -3,13 +3,68 @@ const NeuronalNetworkListener = require('./NeuronalNetworkListener');
 /**
  * Ce listener gere l'event play. Il doit activer les calculs et repondre au croupier dans les temps impartis
  */
-class PlayListener extends EventEmitter {
+class PlayerPlayedListener extends EventEmitter {
   
 
     static handleMessage(message,emmittingPlayer){
-        
+        /*
+        arbre decision:
+
+        joueur non en cour joue => erreur
+
+
+        joue < 0 => erreur
+        joue plus que chips restant => erreur
+        joue chips restant => all in
+                
+                
+        max mise == 0
+                joue 0 => check
+                joue 0 < < chips restant => raise
+                
+                
+        max mise > 0
+                joue 0 => FOLD
+                joue max mise => check
+                joue max mise < < chips restant => raise
+
+        */
+
+        //joueur imprevu a joue=> KO
+        if(emmittingPlayer.details.id !== config.CURR_PLAYER.details.id){
+            console.log('\n Le joueur avec (id = '+ emmittingPlayer.details.id +') a joue MAIS ce n est pas son tour !!');
+            config.PLAYERS.every(function (player){
+                if(player.details.id === emmittingPlayer.details.id){
+                    player.details.state = 'FOLDED';
+                    return true;
+                }
+            })
+        }
+        //joueur joue moins que 0 => KO
+        if(message.data.action.value < 0 ){
+            console.log('\n Le joueur avec (id = '+ emmittingPlayer.details.id +') a misé moins que 0 chips !!');
+            config.PLAYERS.every(function (player){
+                if(player.details.id === emmittingPlayer.details.id){
+                    player.details.state = 'FOLDED';
+                    return true;
+                }
+            })
+        }
+
+        //joueur joue plus que ses chips restants => KO
+        if(message.data.action.value > 0 ){
+            console.log('\n Le joueur avec (id = '+ emmittingPlayer.details.id +') a misé moins que 0 chips !!');
+            config.PLAYERS.every(function (player){
+                if(player.details.id === emmittingPlayer.details.id){
+                    player.details.state = 'FOLDED';
+                    return true;
+                }
+            })
+        }
+
+
+
         //joueur courant a joue => OK
-        if(emmittingPlayer.details.id === config.CURR_PLAYER.details.id){
             console.log('\n Le joueur courant (id = '+ emmittingPlayer.details.id +') a joue ');
             //on initialise sa mise à 0 si pas encore de mise pour ce joueur
             config.CURRENT_BETS.set(emmittingPlayer.details.id,config.CURRENT_BETS.get(emmittingPlayer.details.id)?config.CURRENT_BETS.get(emmittingPlayer.details.id):0);
@@ -19,9 +74,6 @@ class PlayListener extends EventEmitter {
                 //le joueur se couche
                 emmittingPlayer.details.state = 'FOLDED';
                 }
-                config.CURR_PLAYER_VALID_ANSWER = true;
-                clearTimeout(config.CURR_PLAYER_CHRONO);
-                return true;
             }
             if(message.data.action.value === emmittingPlayer.details.chips){
                 //le joueur fait ALL_IN
@@ -29,22 +81,16 @@ class PlayListener extends EventEmitter {
                 emmittingPlayer.details.chips = 0;
                 config.CURRENT_BETS.set(emmittingPlayer.details.id,config.CURRENT_BETS.get(emmittingPlayer.details.id)?config.CURRENT_BETS.get(emmittingPlayer.details.id):0 + message.data.action.value);
                 config.CURRENT_BETS.set('POT',config.CURRENT_BETS.get('POT') + message.data.action.value);
-                if(config.CURRENT_MAX_BET < message.data.action.value){
-                    config.CURRENT_MAX_BET = message.data.action.value;
+                if(config.CURRENT_MAX_BET < config.CURRENT_BETS.get(emmittingPlayer.details.id)){
+                    config.CURRENT_MAX_BET = config.CURRENT_BETS.get(emmittingPlayer.details.id);
                 }
-                config.CURR_PLAYER_VALID_ANSWER = true;
-                clearTimeout(config.CURR_PLAYER_CHRONO);
-                return true;
             }
             if(message.data.action.value < emmittingPlayer.details.chips){
                 //le joueur joue moins que la totalite de ses chips
                 //on verifie qu'il joue au moins la mise max de la main
-                if(message.data.action.value < config.CURRENT_MAX_BET){
+                if(config.CURRENT_BETS.get(emmittingPlayer.details.id) + message.data.action.value < config.CURRENT_MAX_BET){
                     //action invalide => le joueur est FOLDED
                     emmittingPlayer.details.state = 'FOLDED';
-                    config.CURR_PLAYER_VALID_ANSWER = true;
-                    clearTimeout(config.CURR_PLAYER_CHRONO);
-                    return true;
                 } else {
 
                 }
@@ -52,20 +98,13 @@ class PlayListener extends EventEmitter {
                 emmittingPlayer.details.chips = 0;
                 config.CURRENT_BETS.set(emmittingPlayer.details.id,config.CURRENT_BETS.get(emmittingPlayer.details.id)?config.CURRENT_BETS.get(emmittingPlayer.details.id):0 + message.data.action);
                 config.CURRENT_BETS.set('POT',config.CURRENT_BETS.get('POT') + message.data.action);
-                config.CURR_PLAYER_VALID_ANSWER = true;
-                clearTimeout(config.CURR_PLAYER_CHRONO);
-                return true;
             }
         clearTimeout(config.CURR_PLAYER_CHRONO);
         config.CURR_PLAYER_VALID_ANSWER = true;
         return true;
-
-        }
-        //joueur imprevu a joue=> KO
-        else{
-            console.log('\n Le joueur courant (id = '+ emmittingPlayer.details.id +') a joue MAIS ce n est pas son tour !!');
-        }
+        
+        
     }
 }
 
-module.exports = PlayListener;
+module.exports = PlayerPlayedListener;
