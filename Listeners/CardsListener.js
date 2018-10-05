2be2e66a20ef1dce31c0d1770e29494d47a1677b
nammshub <vincent.dhainaut@gmail.com>
2018-10-05 08:33:49 +0200

    tri cartes selon cardInput

--- Listeners/CardsListener.js
+++ Listeners/CardsListener.js
@@ -7,12 +7,28 @@ require('../config');
 class CardsListener extends EventEmitter {
     handleMessage(cardsMessage, playerMemo) {
         playerMemo.hand = cardsMessage.data.cards;
+        //tri croissant selon cardInput
+        playerMemo.hand.sort(this.compare);
+        this.cardsToNeuronalInput(playerMemo)
         /*config.NEURONAL_NETWORK_LISTENER.launchCompute( function(output){
             //game
             console.log('after response from NEURONAL_NETWORK_LISTENER.launchCompute = ' + output);
         });*/
         console.log('inside CardsListener game = '+JSON.stringify(playerMemo));
     }
+
+    cardsToNeuronalInput(playerMemo){
+        playerMemo.turnsDetails[playerMemo.totalHands].h1 = playerMemo.hand[0].cardInput;
+        playerMemo.turnsDetails[playerMemo.totalHands].h2 = playerMemo.hand[1].cardInput;
+    }
+
+    compare(a,b) {
+        if (a.cardInput < b.cardInput)
+          return -1;
+        if (a.cardInput > b.cardInput)
+          return 1;
+        return 0;
+      }
 }
 
 module.exports = CardsListener;
