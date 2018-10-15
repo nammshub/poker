1a1fb6271b03fb8bf15f32a3ec55e6553f49d0ea
nammshub <vincent.dhainaut@gmail.com>
2018-10-15 20:07:32 +0200

    pushNeuronalInput

--- Listeners/ServerPlayerActionListener.js
+++ Listeners/ServerPlayerActionListener.js
@@ -6,6 +6,8 @@ class ServerPlayerActionListener {
         const chipsPlayed = message.action.value;
         const playerId = message.id;
         this.pushPlayerBets(playerId, chipsPlayed, playerMemo);
+        //ajout du coup dans notre neuronalInput
+        this.pushNeuronalInput(playerId, chipsPlayed, playerMemo);
     }
 
     static pushPlayerBets(playerId, chipsPlayed, playerMemo) {
@@ -50,10 +52,37 @@ class ServerPlayerActionListener {
                     break;
             }
             console.log(" mises pour la phase " + posString);
-            currArray.forEach(function(chips){
-                console.log(" "+chips+" ");
+            currArray.forEach(function (chips) {
+                console.log(" " + chips + " ");
             })
         })
+    }
+
+    static pushNeuronalInput(playerId, chipsPlayed, playerMemo) {
+        //on recupere la position du joueur pour ce tour
+        const playerPos = playerMemo.turnsDetails[playerMemo.turnsDetails.length - 1].positionMap.get(playerId);
+        //on recupere le code step turn a injecter
+        const stepNbr = playerMemo.turnsDetails[playerMemo.turnsDetails.length - 1].turnStep;
+        let step;
+        switch (stepNbr) {
+            case 0:
+                step = "pf"
+                break;
+            case 1:
+                step = "f"
+                break;
+            case 2:
+                step = "t"
+                break;
+            case 3:
+                step = "r"
+                break;
+        }
+        //nbr d'action pour ce joueur pour ce step
+        let actionNbr = playerMemo.turnsDetails[playerMemo.turnsDetails.length - 1].betsMap.get(playerId)[stepNbr].length;
+        let neuronalChips = chipsPlayed / playerMemo.potTotal;
+        //on injecte dans playerMemo cette action
+        playerMemo.turnsDetails[playerMemo.turnsDetails.length - 1].neuronalInput.input["p"+playerPos+"_"+step+"_"+actionNbr] = neuronalChips;
     }
 }
 
