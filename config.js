8eaba87898444274e6954295193525db9e119202
nammshub <vincent.dhainaut@gmail.com>
2018-10-21 08:58:13 +0200

    new training running

--- config.js
+++ config.js
@@ -10,18 +10,19 @@ global.config = {
     //constantes partagées
     MAX_SEC_TO_ANSWER: 15,
     MAX_RAISE_MULTIPLIER:10,
-    DISABLE_NEURONAL:true,
-    TRAINING_NEEDED:false,
-    TRAINING_ITERATION: 20000,
+    DISABLE_NEURONAL:false,
+    TRAINING_NEEDED:true,
+    //200000 => environ 30 minutes de training
+    TRAINING_ITERATION: 600000,
 
 
 
     //croupier
     NB_PLAYERS: 0,
-    START_MONEY: 1500,
+    START_MONEY: 150000,
     WAIT_BEFORE_START: 30,
     CURRENT_HAND: 0,
-    MAX_HANDS: 150,
+    MAX_HANDS: 15000,
     ALL_COLORS: ["SPADE", "HEART", "DIAMOND", "CLUB"],
     ALL_KINDS: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "1"],
     CARDS_VALUE_MAP: new Map([
